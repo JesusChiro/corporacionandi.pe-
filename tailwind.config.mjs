535633f39cb44c2952ae19adfaf73de479c01930
JesusChiro <jesus_chiroque@hotmail.com>
2024-10-16 21:36:07 -0500

    Improve navbar desktop version, Adding all images and all screenshots

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#272727',
+				'secondary': '#fff',
+				'logoGreen': '#69B025',
+				'logoBlue': '#245AA4',
+				'logodark': '#0A195F',
+				'fondolight': '#FFFFFF',
+				'fondodark': '#252626',
+
+			}
+		},
 	},
 	plugins: [animations],
 }
