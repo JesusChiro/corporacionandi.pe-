ddd5eb91a8b52a45a611b8f792f65c80020f4a9b
JesusChiro <jesus_chiroque@hotmail.com>
2024-10-18 21:09:39 -0500

    Finish section customers

---
+++ src/consts/Customers.ts
@@ -0,0 +1,57 @@
+export const CUSTOMERS = [
+  {
+    id: "prodac",
+    name: "Prodac",
+  },
+  {
+    id: "siderperu",
+    name: "SiderPerú",
+  },
+  {
+    id: "quimicasuiza",
+    name: "Química Suiza",
+  },
+  {
+    id: "hidrostal",
+    name: "Hidrostal",
+  },
+  {
+    id: "acerosarequipa",
+    name: "Aceros Arequipa",
+  },
+];
+
+export const ALIANCES = [
+  {
+    id: "hiab",
+    name: "HIAB",
+  },
+  {
+    id: "hino",
+    name: "Hino",
+  },
+  {
+    id: "hunter",
+    name: "Hunter",
+  },
+  {
+    id: "international",
+    name: "International",
+  },
+  {
+    id: "hyundai",
+    name: "Hyundai",
+  },
+  {
+    id: "comsatel",
+    name: "Comsatel",
+  },
+  {
+    id: "motored",
+    name: "Motored",
+  },
+  {
+    id: "thermoking",
+    name: "Thermoking",
+  },
+];
